2f44bf11b46d48790f916057c4d8e9d70939284f
Christopher M. Fuhrman <cfuhrman@panix.com>
2008-05-27 21:03:52 +0000

    Import of CFUHRMAN/Log-Fine-0.13 from CPAN.
    gitpan-cpan-distribution: Log-Fine
    gitpan-cpan-version:      0.13
    gitpan-cpan-path:         CFUHRMAN/Log-Fine-0.13.tar.gz
    gitpan-cpan-author:       CFUHRMAN
    gitpan-cpan-maturity:     released

--- lib/Log/Fine.pm
+++ lib/Log/Fine.pm
@@ -77,7 +77,7 @@ use Log::Fine::Logger;
 use Storable qw( dclone );
 use Sys::Syslog qw( :macros );
 
-our $VERSION = '0.12.1';
+our $VERSION = '0.13';
 our @ISA     = qw( Exporter );
 
 =head2 Log Levels
@@ -426,7 +426,7 @@ L<http://search.cpan.org/dist/Log-Fine>
 
 =head1 REVISION INFORMATION
 
-  $Id: Fine.pm 70 2008-05-27 20:48:17Z cfuhrman $
+  $Id: Fine.pm 73 2008-05-27 21:01:20Z cfuhrman $
 
 =head1 COPYRIGHT & LICENSE
 
